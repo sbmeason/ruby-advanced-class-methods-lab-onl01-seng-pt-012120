e84347e7a45500e96508f7c80a65f61c1c34d5ff
Samantha Meason <sbmeason@gmail.com>
2020-02-14 01:32:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,5 +47,10 @@ end
   end
   
   def self.create_from_filename
-    
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
 end
