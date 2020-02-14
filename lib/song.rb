4b3005505761f88d5a32d96d8cbad7c0ecd7d97c
Samantha Meason <sbmeason@gmail.com>
2020-02-14 01:29:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,4 +34,8 @@ end
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
+  
+  def self.alphabetical
+    @@all.sort_by{|x| x.name}
+  end
 end
