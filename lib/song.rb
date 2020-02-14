dccf3162c113c899c273dacd6eeda7f70c22814e
Samantha Meason <sbmeason@gmail.com>
2020-02-14 01:23:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,21 +14,24 @@ class Song
     self.class.all << self
   end
 
-def self.create
+  def self.create
   song = self.new
 end
 
- def self.new_by_name(name)
+  def self.new_by_name(name)
     song = self.new 
     song.name = name
     song
   end
 
-def self.create_by_name(name)
+  def self.create_by_name(name)
     song = self.new 
     song.name = name
     @@all << song
     song
   end
 
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
 end
